6b8771c93f412ee23520ed37c3c60e930ed8ca47
FabiusDev <fsantos93x@gmail.com>
2024-02-04 14:58:49 -0300

    new Slide menu

--- js/script.js
+++ js/script.js
@@ -1,29 +1,36 @@
-var swiper = new Swiper(".slide-content", {
-      slidesPerView: 3,
-      spaceBetween: 25,
-      loop: true,
-      centerSlide: 'true',
-      fade: 'true',
-      grabCursor: 'true',
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-  
-      breakpoints:{
-          0: {
-              slidesPerView: 1,
-          },
-          520: {
-              slidesPerView: 2,
-          },
-          950: {
-              slidesPerView: 3,
-          },
-      },
-    });
+const controls = document.querySelectorAll('.control');
+
+let currentItem = 0;
+const items = document.querySelectorAll('.item');
+
+const maxItens = items.length;
+
+controls.forEach(control => {
+  control.addEventListener('click', () => {
+    const isLeft = control.classList.contains('arrow-left')
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItens) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItens - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
+    })
+
+    items[currentItem].classList.add("current-item");
+
+  })
+})
